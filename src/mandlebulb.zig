958a621433e17bcc641568cfc58ed92c423a7e52
thrombe <yash.n.thombare620@gmail.com>
2025-01-15 01:12:42 +0530

    mandlebulb: update gui

--- src/mandlebulb.zig
+++ src/mandlebulb.zig
@@ -542,39 +542,43 @@ pub const GuiState = struct {
     fn editState(self: *@This(), state: *AppState) void {
         _ = self;
 
+        var reset = false;
+
         _ = c.ImGui_SliderFloat("Speed", &state.camera.speed, 0.1, 10.0);
         _ = c.ImGui_SliderFloat("Sensitivity", &state.camera.sensitivity, 0.001, 2.0);
 
-        if (c.ImGui_Button("Reset time")) {
-            state.time = 0.0;
-            state.deltatime = 0.0;
-            state.frame = 0;
-        }
+        reset = reset or c.ImGui_Button("Reset render state");
 
-        _ = c.ImGui_ColorEdit3("Background Color 1", @ptrCast(&state.background_color1), c.ImGuiColorEditFlags_Float);
-        _ = c.ImGui_ColorEdit3("Background Color 2", @ptrCast(&state.background_color2), c.ImGuiColorEditFlags_Float);
-        _ = c.ImGui_ColorEdit3("trap color 1", @ptrCast(&state.trap_color1), c.ImGuiColorEditFlags_Float);
-        _ = c.ImGui_ColorEdit3("trap color 2", @ptrCast(&state.trap_color2), c.ImGuiColorEditFlags_Float);
-        _ = c.ImGui_ColorEdit3("emission color 1", @ptrCast(&state.emission_color1), c.ImGuiColorEditFlags_Float);
-        _ = c.ImGui_ColorEdit3("emission color 2", @ptrCast(&state.emission_color2), c.ImGuiColorEditFlags_Float);
+        reset = reset or c.ImGui_ColorEdit3("Background Color 1", @ptrCast(&state.background_color1), c.ImGuiColorEditFlags_Float);
+        reset = reset or c.ImGui_ColorEdit3("Background Color 2", @ptrCast(&state.background_color2), c.ImGuiColorEditFlags_Float);
+        reset = reset or c.ImGui_ColorEdit3("trap color 1", @ptrCast(&state.trap_color1), c.ImGuiColorEditFlags_Float);
+        reset = reset or c.ImGui_ColorEdit3("trap color 2", @ptrCast(&state.trap_color2), c.ImGuiColorEditFlags_Float);
+        reset = reset or c.ImGui_ColorEdit3("emission color 1", @ptrCast(&state.emission_color1), c.ImGuiColorEditFlags_Float);
+        reset = reset or c.ImGui_ColorEdit3("emission color 2", @ptrCast(&state.emission_color2), c.ImGuiColorEditFlags_Float);
 
-        _ = c.ImGui_SliderInt("voxel grid side", @ptrCast(&state.voxels.side), 0, @intCast(state.voxels.max_side));
+        reset = reset or c.ImGui_SliderInt("voxel grid side", @ptrCast(&state.voxels.side), 0, @intCast(state.voxels.max_side));
         _ = c.ImGui_Checkbox("voxel debug view", @ptrCast(&state.voxel_debug_view));
 
-        _ = c.ImGui_SliderFloat3("light dir", @ptrCast(&state.light_dir), 0.0, 1.0);
+        reset = reset or c.ImGui_SliderFloat3("light dir", @ptrCast(&state.light_dir), 0.0, 1.0);
         state.light_dir = state.light_dir.normalize3D();
-        _ = c.ImGui_SliderInt("Fractal iterations", @ptrCast(&state.fractal_iterations), 0, 50);
-        _ = c.ImGui_SliderInt("March iterations", @ptrCast(&state.march_iterations), 0, 1024);
+        reset = reset or c.ImGui_SliderInt("Fractal iterations", @ptrCast(&state.fractal_iterations), 0, 50);
+        reset = reset or c.ImGui_SliderInt("March iterations", @ptrCast(&state.march_iterations), 0, 1024);
         _ = c.ImGui_SliderInt("Gather iterations", @ptrCast(&state.gather_iterations), 0, 4096);
         _ = c.ImGui_SliderFloat("gather step factor", &state.gather_step_factor, 0.01, 2.0);
-        _ = c.ImGui_SliderFloat("march step factor", &state.march_step_factor, 0.01, 16.0);
-        _ = c.ImGui_SliderFloat("escape radius", &state.escape_r, 0.01, 16.0);
-        _ = c.ImGui_SliderFloat("fractal scale", &state.fractal_scale, 0.01, 2.0);
-        _ = c.ImGui_SliderFloat("fractal density", &state.fractal_density, 0.01, 512.0);
-        _ = c.ImGui_SliderFloat("ray penetration factor", &state.ray_penetration_factor, 0.01, 1.0);
-        _ = c.ImGui_SliderInt("GI samples", @ptrCast(&state.gi_samples), 0, 4096);
+        reset = reset or c.ImGui_SliderFloat("march step factor", &state.march_step_factor, 0.01, 16.0);
+        reset = reset or c.ImGui_SliderFloat("escape radius", &state.escape_r, 0.01, 16.0);
+        reset = reset or c.ImGui_SliderFloat("fractal scale", &state.fractal_scale, 0.01, 2.0);
+        reset = reset or c.ImGui_SliderFloat("fractal density", &state.fractal_density, 0.01, 512.0);
+        reset = reset or c.ImGui_SliderFloat("ray penetration factor", &state.ray_penetration_factor, 0.01, 1.0);
+        reset = reset or c.ImGui_SliderInt("GI samples", @ptrCast(&state.gi_samples), 0, 4096);
         _ = c.ImGui_SliderFloat("temporal blend factor", &state.temporal_blend_factor, 0.01, 1.0);
-        _ = c.ImGui_SliderFloat("min background attenuation", &state.min_background_attenuation, 0.0, 1.0);
+        reset = reset or c.ImGui_SliderFloat("min background color contribution", &state.min_background_color_contribution, 0.0, 1.0);
+        _ = c.ImGui_SliderFloat("stylistic aliasing factor", &state.stylistic_aliasing_factor, 0.0, 5.0);
         _ = c.ImGui_SliderFloat("t max", &state.t_max, 0.1, 1000.0);
+
+        if (reset) {
+            _ = state.reset_render_state.fuse();
+            state.reset_time();
+        }
     }
 };
